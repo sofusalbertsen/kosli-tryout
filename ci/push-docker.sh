fa31642fa7eff6d85c1641e0072bb1dc277fef82
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2025-06-03 10:51:43 +0200

    Update Docker build and push scripts to use dynamic APP_NAME variable

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/micronaut-app:latest" &
+docker push "ghcr.io/$docker_username/${APP_NAME}:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/${APP_NAME}:latest" &
 wait
