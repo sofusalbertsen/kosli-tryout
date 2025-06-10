24124b2e6f206d40a987a6c216d536b0ef49dae9
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2025-06-10 08:34:13 +0200

    Add template file argument to flow creation in update-flow.sh

--- ci/update-flow.sh
+++ ci/update-flow.sh
@@ -2,4 +2,4 @@
 # This script creates a new flow in Kosli for a Micronaut application.
 set -e
 
-kosli create flow micronaut --description "My small application" 
+kosli create flow micronaut --description "My small application" --template-file=kosli/trail.yaml 
